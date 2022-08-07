6246e04ee609d55b3c82ae39c7bf73bfa6fc4880
riley <vishhh0320@gmail.com>
2022-08-07 11:59:16 +0530

    added imagelist and moved to asset to src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Components/header.js";
 import "./App.css";
 import { useState } from "react";
+import MasonryImageList from "./Components/imagelist.js";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header setSearchQuery={setSearchQuery} setMediaType={setMediaType} />
+      <MasonryImageList />
     </div>
   );
 }
